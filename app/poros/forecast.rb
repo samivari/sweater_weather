66074c74425d47b12a7870b38579ec7176e1008c
Sam Iavri <sammi80134@gmail.com>
2022-04-24 12:13:44 -0600

    forecast poro

---
+++ app/poros/forecast.rb
@@ -0,0 +1,53 @@
+class Forecast
+  attr_reader :id,
+              :current_weather,
+              :hourly_weather,
+              :daily_weather
+
+  def initialize(time_values)
+    @id = nil
+    @current_weather = current(time_values[:current_weather])
+    @hourly_weather = hourly(time_values[:hourly_weather])
+    @daily_weather = daily(time_values[:daily_weather])
+  end
+
+  def current(data)
+    {
+      datetime: Time.at(data[:dt]),
+      sunrise: Time.at(data[:sunrise]),
+      sunset: Time.at(data[:sunset]),
+      temperature: data[:temp],
+      feels_like: data[:feels_like],
+      humidity: data[:humidity],
+      uvi: data[:uvi],
+      visibility: data[:visibility],
+      conditions: data[:weather].first[:description],
+      icon: data[:weather].first[:icon]
+    }
+  end
+
+  def hourly(data)
+    data.map do |hour|
+      {
+        time: Time.at(hour[:dt]).strftime('%T'),
+        temperature: hour[:temp].to_f,
+        conditions: hour[:weather][0][:description],
+        icon: hour[:weather][0][:icon]
+      }
+    end
+  end
+
+  def daily(data)
+    data.map do |day|
+      {
+        date: Time.at(day[:dt]).strftime('%F'),
+        sunrise: Time.at(day[:sunrise]),
+        sunset: Time.at(day[:sunset]),
+        max_temp: day[:temp][:max],
+        min_temp: day[:temp][:min],
+        conditions: day[:weather][0][:description],
+        icon: day[:weather][0][:icon]
+      }
+    end
+  end
+end
