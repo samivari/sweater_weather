e44a6abf4032dbe22c447aee13f30ae1392870b8
Sam Iavri <sammi80134@gmail.com>
2022-04-25 09:20:46 -0600

    update arguments

--- spec/facades/directions_facade_spec.rb
+++ spec/facades/directions_facade_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 RSpec.describe DirectionFacade do
   it '.find_travel_time', :vcr do
-    travel_time = DirectionFacade.find_travel_time('Denver,Co')
+    travel_time = DirectionFacade.find_travel_time('Denver,Co', 'Pueblo,Co')
 
     expect(travel_time).to be_a(Direction)
   end
