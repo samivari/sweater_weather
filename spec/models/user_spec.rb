acf6b46852866e1ab65359c0aea32688447b1189
Sam Iavri <sammi80134@gmail.com>
2022-04-25 17:57:32 -0600

    user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validataions' do
+    it { should validate_presence_of :email }
+    it { should validate_uniqueness_of :email }
+    it { should validate_presence_of :password_confirmation }
+    it { should have_secure_password }
+  end
+
+  describe 'user create' do
+    it 'creates user object with all attributes' do
+      user = User.create!(email: 'sam@aol.com', password: 'password', password_confirmation: 'password')
+      expect(user).to have_attributes(email: 'sam@aol.com')
+      expect(user).to have_attributes(password: 'password')
+      expect(user).to have_attributes(password_confirmation: 'password')
+      expect(user).to have_attribute(:api_key)
+    end
+  end
 end
