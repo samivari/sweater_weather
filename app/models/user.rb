db37735392832e05b3d9a5657dc0261f3934cbe2
Sam Iavri <sammi80134@gmail.com>
2022-04-25 17:58:12 -0600

    User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  before_create :create_key
+  has_secure_password
+  has_secure_token :api_key
+  validates :email, :password_digest, :password_confirmation, presence: true
+  validates_uniqueness_of :email
+
+  def create_key
+    self.api_key = SecureRandom.hex(20)
+  end
 end
