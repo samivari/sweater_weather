c492df5e70f4d65ac3ac47495b61b2deb4429a6f
Sam Iavri <sammi80134@gmail.com>
2022-04-25 09:19:07 -0600

    directions api consumption spec

---
+++ spec/facades/directions_facade_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+RSpec.describe DirectionFacade do
+  it '.find_travel_time', :vcr do
+    travel_time = DirectionFacade.find_travel_time('Denver,Co')
+
+    expect(travel_time).to be_a(Direction)
+  end
+end
