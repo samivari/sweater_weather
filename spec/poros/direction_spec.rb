9d885706def56949b9429e76d3da6db16236b5bc
Sam Iavri <sammi80134@gmail.com>
2022-04-25 09:48:28 -0600

    Direction poro and spec

---
+++ spec/poros/direction_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+RSpec.describe 'Location Poro' do
+  it 'takes JSON response and create ruby object', :vcr do
+    data = DirectionService.get_travel_time('Denver,CO', 'Pueblo,CO')
+    direction = Direction.new(data)
+
+    expect(direction).to be_a(Direction)
+    expect(direction.travel_time).to eq('01:45:23')
+  end
+end
