a494e447c7360bfdb6fcc4e4e24d1605cd4eda95
Sam Iavri <sammi80134@gmail.com>
2022-04-25 21:01:09 -0600

    sad path, password is invalid

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,10 +2,12 @@ class Api::V1::SessionsController < ApplicationController
   def create
     params[:email] = JSON.parse(request.raw_post)['email']
     params[:password] = JSON.parse(request.raw_post)['password']
-
     user = User.find_by(email: params[:email])
-    user.authenticate(params[:password])
-    session[:user_id] = user.id
-    render json: UserSerializer.new(user)
+    if user.authenticate(params[:password])
+      session[:user_id] = user.id
+      render json: UserSerializer.new(user)
+    else
+      render json: { status: 'ERROR', message: 'Invalid username or password', data: {} }, status: :bad_request
+    end
   end
 end
