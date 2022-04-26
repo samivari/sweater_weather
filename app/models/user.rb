790888c6c15c6c7b528bfe1aefcc04996f5f4ed9
Sam Iavri <sammi80134@gmail.com>
2022-04-25 19:55:48 -0600

    restructure user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ApplicationRecord
   before_create :create_key
   has_secure_password
   has_secure_token :api_key
-  validates :email, :password_digest, :password_confirmation, presence: true
+  validates_presence_of :email, email: true
   validates_uniqueness_of :email
+  validates_presence_of :password_digest, confirmation: true
+  validates_presence_of :password_confirmation, presence: true
 
   def create_key
     self.api_key = SecureRandom.hex(20)
