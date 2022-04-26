68b0a20d7ff90c3e686aa0a2180eac4039755a7d
Sam Iavri <sammi80134@gmail.com>
2022-04-25 20:12:02 -0600

    sad paths for passwords

--- spec/api/v1/request/registration_request_spec.rb
+++ spec/api/v1/request/registration_request_spec.rb
@@ -44,4 +44,63 @@ RSpec.describe 'user registration' do
       expect(parsed[:message]).to eq("Password confirmation doesn't match Password")
     end
   end
+
+  it 'registers a user from request  nil PW' do
+    user_params = {
+      "email": 'whatever@example.com',
+      "password": 'password',
+      "password_confirmation": nil
+    }
+    headers = {
+      'Content-Type' => 'application/json',
+      'Accept' => 'application/json'
+    }
+    post '/api/v1/users', headers: headers, params: user_params.to_json
+
+    expect(response).to_not be_successful
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed[:status]).to eq(400)
+    expect(parsed[:message]).to eq("Password confirmation can't be blank")
+  end
+  it 'registers a user from request  no PW' do
+    user_params = {
+      "email": 'whatever@example.com',
+      "password": ' ',
+      "password_confirmation": ' '
+    }
+    headers = {
+      'Content-Type' => 'application/json',
+      'Accept' => 'application/json'
+    }
+    post '/api/v1/users', headers: headers, params: user_params.to_json
+
+    expect(response).to_not be_successful
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed[:status]).to eq(400)
+    expect(parsed[:message]).to eq("Password confirmation can't be blank")
+  end
+  it 'registers a user from request invalid email' do
+    user_params = {
+      "email": 'whateverexample.com',
+      "password": 'password',
+      "password_confirmation": 'password'
+    }
+    headers = {
+      'Content-Type' => 'application/json',
+      'Accept' => 'application/json'
+    }
+    post '/api/v1/users', headers: headers, params: user_params.to_json
+
+    #  expect(response).to_not be_successful
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+    require 'pry'
+    binding.pry
+    expect(parsed[:status]).to eq(400)
+    expect(parsed[:message]).to eq("Password confirmation can't be blank")
+  end
 end
