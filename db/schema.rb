ad0746652508c8cfd59a5d481833446c395a55d0
Sam Iavri <sammi80134@gmail.com>
2022-04-25 19:55:10 -0600

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_25_223839) do
+ActiveRecord::Schema.define(version: 2022_04_26_013321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2022_04_25_223839) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_confirmation"
   end
 
 end
