09d7d41086851b39ebe3e271c56dec0041b33b0c
Sam Iavri <sammi80134@gmail.com>
2022-04-26 11:16:56 -0600

    roadtrip api request spec

--- spec/api/v1/request/road_trip_request_spec.rb
+++ spec/api/v1/request/road_trip_request_spec.rb
@@ -17,11 +17,12 @@ RSpec.describe 'road trip request' do
     post '/api/v1/road_trip', headers: headers, params: user_params.to_json
     expect(response).to be_successful
     parsed = JSON.parse(response.body, symbolize_names: true)
-    expect(parsed[:data][:id]).to eq(nil)
+    expect(parsed[:data][:id]).to eq('null')
     expect(parsed[:data][:type]).to eq('roadtrip')
     expect(parsed[:data][:attributes][:start_city]).to eq('Denver,CO')
     expect(parsed[:data][:attributes][:end_city]).to eq('Pueblo,CO')
-    expect(parsed[:data][:attributes][:travel_time]).to eq('01:45:23')
-    expect(parsed[:data][:attributes][:arrival_weather][:time]).to be_a
+    expect(parsed[:data][:attributes][:travel_time]).to eq('01:48:25')
+    expect(parsed[:data][:attributes][:weather_at_eta][:temperature]).to eq(65.57)
+    expect(parsed[:data][:attributes][:weather_at_eta][:conditions]).to eq('overcast clouds')
   end
 end
