cdf8c5e16b0c1854fdcd893273eca64f4f5af729
Sam Iavri <sammi80134@gmail.com>
2022-04-26 11:17:46 -0600

    road trip poro

---
+++ app/poros/road_trip.rb
@@ -0,0 +1,38 @@
+class RoadTrip
+  attr_reader :start_city, :end_city, :travel_time, :weather
+
+  def initialize(forecast, direction, origin, destination)
+    @start_city = origin
+    @end_city = destination
+    @travel_time = route_verify(direction)
+    @weather = arrival_weather(forecast, travel_time, direction)
+  end
+
+  def time_converter(direction)
+    [direction.travel_time / 3600, direction.travel_time / 60 % 60, direction.travel_time % 60].map do |t|
+      t.to_s.rjust(2, '0')
+    end.join(':')
+  end
+
+  def route_verify(direction)
+    if direction.travel_time.nil?
+      'Impossible route.'
+    else
+      time_converter(direction)
+    end
+  end
+
+  def arrival_weather(forecast, travel_time, direction)
+    if travel_time == 'Impossible route.'
+      'No Data.'
+    else
+      time = Time.new.to_i
+      arrival_time = (direction.travel_time + time.to_i)
+      time = Time.at(arrival_time).to_datetime
+
+      forecast.hourly_weather.find do |h|
+        time.strftime('%H').to_i == (h[:time].to_datetime).strftime('%H').to_i
+      end
+    end
+  end
+end
